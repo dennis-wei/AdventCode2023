209e51d1918a9c280355103bef398314fb4f7dd3
Dennis Wei <dw2654@columbia.edu>
2023-12-06 00:12:48 -0500

    day 6: 662, 701

---
+++ aoc_elixir/solutions/6.exs
@@ -0,0 +1,57 @@
+defmodule Day6 do
+  def get_input(test \\ false) do
+    filename = case test do
+      false -> "input/6.txt"
+      true -> "test_input/6.txt"
+    end
+    Input
+      # .ints(filename)
+      # .line_tokens(filename)
+      .lines(filename)
+      # .line_of_ints(filename)
+  end
+
+  def get_all_nums(s) do
+    Regex.scan(~r/\d+/, s)
+      |> List.flatten
+      |> Enum.map(&String.to_integer/1)
+  end
+
+  def run(times, distance_records) do
+    zipped = Enum.zip(times, distance_records)
+    num_wins = zipped
+      |> Enum.map(fn {t, d} ->
+        Enum.reduce(0..t, 0, fn i, acc ->
+          res = i * (t - i)
+          cond do
+            res > d -> acc + 1
+            true -> acc
+          end
+        end)
+      end)
+    Enum.product(num_wins)
+  end
+
+  def combine(nums) do
+    Enum.reduce(nums, "", fn n, acc ->
+      "#{acc}#{n}"
+    end)
+      |> String.to_integer()
+  end
+
+  def solve(test \\ false) do
+    input = get_input(test)
+    times = input |> Enum.at(0) |> get_all_nums
+    distance_records = input |> Enum.at(1) |> get_all_nums
+    part1 = run(times, distance_records)
+
+    combined_times = combine(times)
+    combined_distnaces = combine(distance_records)
+
+    part2 = run([combined_times], [combined_distnaces])
+    IO.puts("Part 1: #{part1}")
+    IO.puts("Part 2: #{part2}")
+  end
+end
+
+Day6.solve()
