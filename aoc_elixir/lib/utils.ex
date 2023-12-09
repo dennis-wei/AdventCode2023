1cf4559f3c3ea7f77a7299a5fb237107bc4255fc
Dennis Wei <dw2654@columbia.edu>
2023-12-09 10:23:58 -0500

    day 9 (35079, 33957)

--- aoc_elixir/lib/utils.ex
+++ aoc_elixir/lib/utils.ex
@@ -1,6 +1,6 @@
 defmodule Utils do
   def get_all_nums(s) do
-    Regex.scan(~r/\d+/, s)
+    Regex.scan(~r/-?\d+/, s)
       |> List.flatten
       |> Enum.map(&String.to_integer/1)
   end
---
+++ aoc_elixir/solutions/9.exs
@@ -0,0 +1,50 @@
+defmodule Day9 do
+  def get_input(test \\ false) do
+    filename = case test do
+      false -> "input/9.txt"
+      true -> "test_input/9.txt"
+    end
+    Input
+      # .ints(filename)
+      # .line_tokens(filename)
+      .lines(filename)
+      # .line_of_ints(filename)
+  end
+
+  def run_history(nums, part2 \\ false) do
+    initial = cond do
+      part2 -> nums
+      true -> Enum.reverse(nums)
+    end
+    seqs = Enum.reduce_while(1..10000, [initial], fn _i, acc ->
+      diffs = Enum.zip(hd(acc), tl(hd(acc)))
+        |> Enum.map(fn {a, b} -> a - b end)
+      cond do
+        Enum.all?(diffs, &(&1 == 0)) -> {:halt, acc}
+        true -> {:cont, [diffs | acc]}
+      end
+    end)
+    Enum.reduce(1..Enum.count(seqs)-1, seqs, fn _i, acc ->
+      [lower_seq | [next_seq | rem_seqs]] = acc
+      diff = hd(lower_seq)
+      prior = hd(next_seq)
+      [[prior + diff | next_seq] | rem_seqs]
+    end)
+      |> hd
+      |> hd
+
+  end
+
+  def solve(test \\ false) do
+    input = get_input(test)
+      |> Enum.map(fn line -> Utils.get_all_nums(line) end)
+    part1 = Enum.map(input, fn nums -> run_history(nums, false) end)
+      |> Enum.sum
+    part2 = Enum.map(input, fn nums -> run_history(nums, true) end)
+      |> Enum.sum
+    IO.puts("Part 1: #{part1}")
+    IO.puts("Part 2: #{part2}")
+  end
+end
+
+Day9.solve()
