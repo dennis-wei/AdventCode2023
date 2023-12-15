575cc31282976cfab777f0361dbfa7bef4e63628
Dennis Wei <dw2654@columbia.edu>
2023-12-15 01:19:22 -0500

    ok apparently process.put is good - day 12 (2244, 22464)

--- aoc_elixir/solutions/12.exs
+++ aoc_elixir/solutions/12.exs
@@ -26,75 +26,59 @@ defmodule Day12 do
   end
 
 
-  def run_memoized({str, pattern}, curr_block, memo) do
+  def run({str, pattern}, curr_block) do
     key = {str, pattern, curr_block}
-    {res, umap} = cond do
-      str == [] and String.length(curr_block) == 0 and pattern == [] -> {1, Map.put(memo, key, 1)}
-      str == [] and String.length(curr_block) != 0 and pattern == [] -> {0, Map.put(memo, key, 0)}
-      str == [] and String.length(curr_block) == hd(pattern) -> {1, Map.put(memo, key, 1)}
-      str == [] and String.length(curr_block) != hd(pattern) -> {0, Map.put(memo, key, 0)}
-      Enum.count(str) + String.length(curr_block) < Enum.sum(pattern) -> {0, Map.put(memo, key, 0)}
-      Enum.count(pattern) > 0 and String.length(curr_block) > hd(pattern) -> {0, Map.put(memo, key, 0)}
+    res = cond do
+      str == [] and String.length(curr_block) == 0 and pattern == [] -> 1
+      str == [] and String.length(curr_block) != 0 and pattern == [] -> 0
+      str == [] and String.length(curr_block) == hd(pattern) -> 1
+      str == [] and String.length(curr_block) != hd(pattern) -> 0
+      Enum.count(str) + String.length(curr_block) < Enum.sum(pattern) -> 0
+      Enum.count(pattern) > 0 and String.length(curr_block) > hd(pattern) -> 0
       true ->
-        case Map.get(memo, key) do
+        case Process.get(key) do
           :nil ->
             case hd(str) do
               "." ->
                 cond do
-                  String.length(curr_block) > 0 and pattern == [] -> {0, Map.put(memo, key, 0)}
-                  String.length(curr_block) > 0 and String.length(curr_block) != hd(pattern) -> {0, Map.put(memo, key, 0)}
+                  String.length(curr_block) > 0 and pattern == [] -> 0
+                  String.length(curr_block) > 0 and String.length(curr_block) != hd(pattern) -> 0
                   String.length(curr_block) > 0 and String.length(curr_block) == hd(pattern) ->
-                    run_memoized({tl(str), tl(pattern)}, "", memo)
-                  String.length(curr_block) == 0 -> run_memoized({tl(str), pattern}, "", memo)
+                    run({tl(str), tl(pattern)}, "")
+                  String.length(curr_block) == 0 -> run({tl(str), pattern}, "")
                 end
-              "#" -> run_memoized({tl(str), pattern}, "#{curr_block}#", memo)
+              "#" -> run({tl(str), pattern}, "#{curr_block}#")
               "?" ->
-                {r1, m1} = run_memoized({["." | tl(str)], pattern}, curr_block, memo)
-                {r2, m2} = run_memoized({["#" | tl(str)], pattern}, curr_block, memo)
-                {r1 + r2, Map.merge(m1, m2)}
+                r1 = run({["." | tl(str)], pattern}, curr_block)
+                r2 = run({["#" | tl(str)], pattern}, curr_block)
+                r1 + r2
             end
-          n -> {n, memo}
+          n -> n
         end
     end
-    # {key, res} |> IO.inspect
-    {res, Map.put(umap, key, res)}
+    Process.put(key, res)
+    res
   end
 
   def solve(test \\ false) do
     base_input = get_input(test)
     part1_input = base_input
       |> Enum.map(fn [str, pattern] -> {String.graphemes(str), Utils.get_all_nums(pattern)} end)
-    {part1, cache} = part1_input
-      |> Enum.reduce({0, %{}}, fn line, {racc, macc} ->
-        {nr, nacc} = run_memoized(line, "", macc)
-        {racc + nr, nacc}
+    part1 = part1_input
+      |> Enum.reduce(0, fn line, acc ->
+        acc + run(line, "")
       end)
-    # part2 = base_input
-    #   |> Enum.map(fn line -> unfold(line) end)
-    #   |> Enum.map(fn [str, pattern] -> {String.graphemes(str), Utils.get_all_nums(pattern)} end)
-    #   |> Enum.map(fn line ->
-    #     IO.puts("Starting #{Enum.join(line |> elem(0), "")}")
-    #     res = run_memoized(line, "", %{})
-    #     IO.puts("Done with #{Enum.join(line |> elem(0), "")}")
-    #     res
-    #   end)
-    #   |> Enum.map(fn {nr, _} -> nr end)
-    #   |> Enum.sum
-      # |> Enum.reduce({0, cache}, fn line, {racc, macc} ->
-      #   IO.puts("Starting #{Enum.join(line |> elem(0), "")}")
-      #   {nr, nacc} = run_memoized(line, "", macc)
-      #   IO.puts("Done with #{Enum.join(line |> elem(0), "")}")
-      #   Enum.count(nacc) |> IO.inspect(label: "Cache size")
-      #   {racc + nr, nacc}
-      # end)
-      # |> elem(0)
-    part2 = nil
+    part2 = base_input
+      |> Enum.map(fn line -> unfold(line) end)
+      |> Enum.map(fn [str, pattern] -> {String.graphemes(str), Utils.get_all_nums(pattern)} end)
+      |> Enum.map(fn line ->
+        res = run(line, "")
+        res
+      end)
+      |> Enum.sum
     IO.puts("Part 1: #{part1}")
     IO.puts("Part 2: #{part2}")
   end
 end
 
-:timer.tc(fn -> Day12.solve() end, [])
-  |> elem(0)
-  |> Kernel./(1_000_000)
-  |> IO.inspect
+Day12.solve()
